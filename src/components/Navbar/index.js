4a14c2e1314a34528c555140c1f7726d0c2c18b3
shine <shine401020@gmail.com>
2024-06-26 12:54:29 +0900

    fix: add modal for redirect

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,57 @@
+import { Box, Button, Modal, Typography } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const navigate = useNavigate();
+
   return (
     <div className="absolute top-14 m-3 sm:left-10">
-      <Link to="/">
-        <div>
-          <img
-            src={`/assets/images/icon.png`}
-            alt="Bible"
-            className="w-8 md:w-16 lg:w-20"
-          />
+      <div onClick={handleOpen}>
+        <img
+          src={`/assets/images/icon.png`}
+          alt="Bible"
+          className="w-8 md:w-16 lg:w-20"
+        />
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className="flex flex-col items-middle h-screen">
+          <div className="min-w-72 w-1/2 bg-white m-auto p-7">
+            <div className="text-center text-sm lg:text-lg py-7">
+              Are you sure you want to leave the current Bible Verse Master
+              session?
+            </div>
+            <div className="flex justify-between md:justify-end gap-3">
+              <Button
+                className="text-sm lg:text-lg"
+                sx={{fontSize: "12px", padding: "5px"}}
+                variant="contained"
+                color="primary"
+                onClick={() => navigate("/")}
+              >
+                Yes
+              </Button>
+              <Button
+                className="text-sm lg:text-lg"
+                sx={{fontSize: "12px", padding: "5px"}}
+                variant="contained"
+                color="error"
+                onClick={handleClose}
+              >
+                No
+              </Button>
+            </div>
+          </div>
         </div>
-      </Link>
+      </Modal>
     </div>
   );
 }
